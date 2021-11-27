4f5d08574fbc30802e798382a8a8033ab4ffc363
L. Guruprasad <lgp171188@gmail.com>
2021-11-27 12:38:50 +0530

    Implement subtraction
    Also add unit tests for the same

--- operations.py
+++ operations.py
@@ -8,3 +8,10 @@ def add(number1, number2):
     Add 'number1' and 'number2' and return the result.
     """
     return number1 + number2
+
+
+def subtract(number1, number2):
+    """
+    Subtract 'number2' from 'number1' and return the result.
+    """
+    return number1 - number2
